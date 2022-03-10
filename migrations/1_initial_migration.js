c6925752ea85aaae398d128a82de6ae12a2e4a81
Michael <michael.wong@epitech.eu>
2022-03-10 23:33:56 +0000

    Add voting to migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const Voting = artifacts.require('Voting');
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Voting);
 };
