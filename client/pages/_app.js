a9ab11ef4f63cbfdf8ff03561c2e5ef911f43593
Michael <michael.wong@epitech.eu>
2022-03-10 21:28:58 +0000

    Fix ID Moralis

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,8 +5,8 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <MoralisProvider
-      appId='https://wvzfjosoxxda.usemoralis.com:2053/server'
-      serverUrl='axnyKWbfuEcU44UqR2Hi8w30RDGKOm1L4pizyuQd'
+      appId='axnyKWbfuEcU44UqR2Hi8w30RDGKOm1L4pizyuQd'
+      serverUrl='https://wvzfjosoxxda.usemoralis.com:2053/server'
     >
       <Component {...pageProps} />
     </MoralisProvider>
