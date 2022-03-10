c41ff9346889f47704cb91b5326e0e2390a935f3
Michael <michael.wong@epitech.eu>
2022-03-10 17:43:41 +0000

    Add connexion Metamsk in Index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,55 +1,54 @@
-// import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useMoralis } from 'react-moralis';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import { Container,Row,Col } from 'react-bootstrap';
 
-const Home = () => {
+function Home(){
   const { isAuthenticated, authenticate } = useMoralis();
   const router = useRouter();
 
-  useEffect(() => {
-    if (isAuthenticated) router.replace("/dashboard");
-  }, [isAuthenticated]);
-
-  return (
-    <>
-    <Head>
-      <title>Nobodyw Dapp</title>
-      <meta name="description" content="Blockchain dapp" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <Container>
-      <Row>
-        <Col>
-          <div className='justify-center flex' id='titre'>
-            <h1>Bienvenue sur Blockchain Dapp !</h1>
-          </div>
-       </Col>
-      </Row>
-    </Container>
-
-    <div className="flex items-center justify-center">
-
+  if(!isAuthenticated){
+    return (
+      <>
+      <Head>
+        <title>Nobodyw Dapp</title>
+        <meta name="description" content="Blockchain dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  
       <Container>
         <Row>
           <Col>
-            <button
-              onClick={() =>
-                authenticate({ signingMessage: "authorize your metamsk to access the application" })
-              }
-              className="px-7 py-4 text-xl rounded-xl bg-yellow-300 animate-pulse"
-            >
-              Connecte toi avec Metamask
-            </button>
-      </Col>
+            <div className='justify-center flex' id='titre'>
+              <h1>Bienvenue sur Blockchain Dapp !</h1>
+            </div>
+         </Col>
         </Row>
       </Container>
-    </div>
-    </>
-  )
+  
+      <div className="flex items-center justify-center">
+        <Container>
+          <Row>
+            <Col>
+              <button
+                onClick={() =>
+                  authenticate({ signingMessage: "authorize your metamsk to access the application" })
+                }
+                className="px-7 py-4 text-xl rounded-xl bg-yellow-300 animate-pulse"
+              >
+                Connecte toi avec Metamask
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      </>
+    )
+  }
+  else{
+    router.replace('/dashboard');
+    return(<p></p>);
+  }
 }
 
 export default Home
