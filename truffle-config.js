10db996103f6540faa45db5e574ef4cf0acd3aa0
Michael <michael.wong@epitech.eu>
2022-03-11 00:12:43 +0000

    Create new path Artifacts and add to .gitignore

--- truffle-config.js
+++ truffle-config.js
@@ -25,15 +25,9 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
+  
+  contracts_build_directory: "./client/artifacts",
+
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -90,7 +84,7 @@ module.exports = {
         },
         evmVersion: "byzantium"
       }
-    }
+    },
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
